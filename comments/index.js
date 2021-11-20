1a3459f04f69275f3bc1830b5473f037a6ee6c79
Boris <boris.shulman@tabit.cloud>
2021-11-20 13:57:52 +0200

    Add Create Post on Client

--- comments/index.js
+++ comments/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
