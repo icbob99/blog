fdc49e0896e14d6c682095137c2219fd1e4725bd
Boris <boris.shulman@tabit.cloud>
2021-11-20 13:58:30 +0200

    Add Post on Client

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 // app.use(bodyParser.json(0));
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(cors());
 
 const posts = {};
 
