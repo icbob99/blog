c6fb6e8591d76e1e3efe908f16f8028e6e0e80f3
Boris <boris.shulman@tabit.cloud>
2021-11-20 15:53:22 +0200

    Add comments to post

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate.js";
+import CommentList from "./CommentList.js";
 
 export default () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
         const res = await axios.get('http://localhost:4000/posts');
+
         setPosts(res.data);
     };
 
@@ -17,6 +20,9 @@ export default () => {
         return <div className="card" style={{ width: '30%', marginBottom: '20px' }} key={post.id}>
             <div className="card-body">
                 <h3>{post.title}</h3>
+                <CommentList postId={post.id} />
+                <CommentCreate postId={post.id} />
+
             </div>
         </div>
     });
