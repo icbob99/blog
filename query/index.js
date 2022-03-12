0d440f6e193ddff75943afcfa1594636f9faae9d
Boris <boris.shulman@tabit.cloud>
2022-03-12 11:19:35 +0200

    12032022 - commit before continue

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,14 +17,39 @@ app.post('/events', (req, res) => {
     const { type, data } = req.body;
     console.log('Event received:', req.body.type);
 
+    handleEvent(type, data);
+
+    res.send({});
+});
+
+app.listen(4002, async () => {
+    console.log('listining on 4002');
+
+    try {
+        const res = await axios.get("http://localhost:4005/events");
+
+        for (let event of res.data) {
+            console.log("Processing event:", event.type);
+
+            handleEvent(event.type, event.data);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+const handleEvent = (type, data) => {
+    
     if (type === 'PostCreated') {
         const { id, title } = data;
         posts[id] = { id, title, comments: [] };
+        console.log(`post => ${JSON.stringify(posts, null, ' ')}`);
     }
 
     if (type === 'CommentCreated') {
         const { id, content, postId, status } = data;
-        
+
         console.log(`id => ${id}, content=> ${content}, postId=>${postId}`);
 
         const post = posts[postId];
@@ -33,9 +59,9 @@ app.post('/events', (req, res) => {
 
     if (type === 'CommentUpdated') {
         const { id, content, postId, status } = data;
-        
+
         console.log(`id => ${id}, content=> ${content}, postId=>${postId}, status=>${status}`);
-        
+
         const post = posts[postId];
         const comment = post.comments.find(comment => {
             return comment.id === id;
@@ -45,12 +71,4 @@ app.post('/events', (req, res) => {
         comment.content = content;
         console.log(`post => ${JSON.stringify(post, null, ' ')}`);
     }
-
-    
-
-    res.send({});
-});
-
-app.listen(4002, () => {
-    console.log('listining on 4002');
-});
+}
